cfab667cd9044b6eb6b1c5b7a02a7dc121eacb7f
Tan Yi Peng <tanyipeng834@gmail.com>
2023-12-12 18:13:55 +0800

    odd even linked list

---
+++ oddEvenList.py
@@ -0,0 +1,16 @@
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        odd = head
+        # Take the second element of the linked list
+        even_head = even = head.next
+        while even and even.next:
+            odd.next = odd.next.next
+            odd = odd.next
+            even.next = even.next.next
+            even = even .next
+            
+        
+        odd.next=even_head
+        return head
