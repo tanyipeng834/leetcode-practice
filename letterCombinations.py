31b58f33a5ad666ccc34f7eb670e4e555ad99788
Tan Yi Peng <tanyipeng834@gmail.com>
2024-01-21 10:49:56 +0800

    Finished letter combinations

---
+++ letterCombinations.py
@@ -0,0 +1,43 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        combinations = []
+        phone_number = { "2" : "abc",
+         "3" : "def",
+         "4":"ghi",
+         "5" : "jkl",
+         "6" : "mno",
+         "7" :"pqrs",
+         "8" : "tuv",
+         "9" :"wxyz",
+        }
+        def backtrack(current_number,digits):
+            if digits =="":
+                if current_number !="":
+                    combinations.append(current_number)
+                return
+            else:
+                digit = digits[0]
+                letters = phone_number[digit]
+                for letter in letters:
+                   
+                    backtrack(current_number + letter,digits[1:])
+                    
+        backtrack("",digits)
+        return combinations
+                
+
+
+          
+
+       
+
+
+
+
+
+
+
+
+
+
+        
